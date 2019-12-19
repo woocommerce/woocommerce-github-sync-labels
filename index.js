4d49ac8ce03a75a5c967fe64c3b9aa4325a93d85
Claudio Sanches <contato@claudiosanches.com>
2019-12-19 20:57:14 -0300

    Added project files

---
+++ index.js
@@ -0,0 +1,41 @@
+const githubLabelSync = require('github-label-sync');
+const fs = require('fs');
+const hasFlag = require('has-flag');
+const filendir = require('filendir')
+
+// Load config, repositories list and common labels.
+const config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+const repositories = JSON.parse(fs.readFileSync('./repositories.json', 'utf8'));
+const commonLabels = JSON.parse(fs.readFileSync('./labels/commons.json', 'utf8'));
+
+// Set command line args.
+let dryRun = hasFlag('--dry-run');
+let preserveLabels = hasFlag('--preserve-labels');
+
+console.log('Starting sync...');
+if (dryRun) {
+  console.log('✔️ "--dry-run" is enabled. No changes will happens in any repository.');
+}
+if (preserveLabels) {
+  console.log('✔️ "--preserve-labels" is enabled. New labels introduced directly in GitHub will be preserved');
+}
+
+repositories.forEach(function(repo) {
+  const projectLabels = './labels/' + repo + '.json';
+  let labels = commonLabels;
+
+  if (fs.existsSync(projectLabels)) {
+    labels = JSON.parse(fs.readFileSync(projectLabels, 'utf8'));
+  }
+
+  githubLabelSync({
+    accessToken: config.githubAccessToken,
+    repo: repo,
+    labels: labels,
+    dryRun: dryRun,
+    allowAddedLabels: preserveLabels
+  }).then((diff) => {
+    filendir.writeFileSync('./logs/' + repo + '.txt', JSON.stringify(diff, null, 2));
+    console.log('Labels synced for "' + repo + '"');
+  });
+});
